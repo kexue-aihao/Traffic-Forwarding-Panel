c1e210c91b9e8c515b0694f268b8a9326d85cc32
Genbic <142807165+kexue-aihao@users.noreply.github.com>
2026-09-26 07:53:23 +0800

    release: publish v0.1.13

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 umask 077
 
-VERSION="0.1.12"
+VERSION="0.1.13"
 RELEASE_URL="https://github.com/kexue-aihao/Traffic-Forwarding-Panel/releases/download/v${VERSION}"
 install_dir=/opt/traffic-forwarding-panel
 port=18080
